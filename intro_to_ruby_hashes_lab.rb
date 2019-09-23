1b68b0503cc0981f4c620ee609072e924b0de891
Deanna Kelley <deanna.georges@gmail.com>
2019-09-23 15:21:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
+  key = :name
   my_hash_creator = {:name => "Grace Hopper", :id => 234}
   my_hash_creator[:name, :id]
   # return a hash that includes the key and value parameters passed into this method
